ff4909b78b0b7db28cf1691c44bf1ea9ddbb3412
mdodard <mdodard@w21.fdsetu.infra.umontpellier.fr>
2018-09-14 14:16:55 +0200

    Fix

---
+++ administration_base_donnees/tp1/tp2.sql
@@ -0,0 +1,23 @@
+rlwrap sqlplus $username/$mdp@venus/master
+
+start ScriptCreation.sql
+
+CREATE OR REPLACE TRIGGER T1
+BEFORE INSERT OR DELETE ON Emp
+FOR EACH ROW
+BEGIN
+  dbms_output.put_line('Yolo.');
+END;
+/
+
+SELECT *
+FROM Emp;
+
+desc user_triggers;
+
+-- show error
+
+SELECT trigger_name FROM user_triggers;
+
+INSERT INTO Emp(nom, num, salaire)
+VALUES ('jean-kevin',  9999, 1789)
