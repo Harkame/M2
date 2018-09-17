78bda807eb2826718859dd2fa43a7d29b3685937
Harkame <louisr.daviaud@gmail.com>
2018-09-17 15:38:13 +0200

    Fix

---
+++ evolution_restructuration/td1/src/FeuilleImpot.java
@@ -0,0 +1,15 @@
+
+public class FeuilleImpot {
+	
+	int format; 
+	FeuilleImpot(int fm){
+		format = fm;
+	}
+	
+	void editer(int categorie){
+		//TODO Switch l'ensemble des cas des imposables
+		
+		// penser au refactoring 
+	}
+
+}
