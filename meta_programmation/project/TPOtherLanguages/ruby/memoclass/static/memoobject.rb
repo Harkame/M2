353cee798f0440e3e9023bcc2bda4c8e62328c24
Harkame <louisr.daviaud@gmail.com>
2018-11-19 10:06:43 +0100

    memoobject

---
+++ meta_programmation/project/TPOtherLanguages/ruby/memoclass/static/memoobject.rb
@@ -0,0 +1,36 @@
+# Class who contains instances
+class MemoObject
+  @@instances = [] #List of instances
+
+  # Default initializer
+  # Store self into instances
+  def initialize
+    @@instances << self
+  end
+
+  ## Getter - instances
+  def self.all_instances
+    @@instances
+  end
+end
+
+# An subclass of MemoObject
+class Person < MemoObject
+
+  # Initializer with parameters (name, age)
+  def initialize name, age
+    super()
+    @name = name
+    @age = age
+  end
+end
+
+# Test
+
+# Creation of 3 persons
+p1 = Person.new "Titi", 12
+p2 = Person.new "Toto", 78
+p3 = Person.new "Tata", 42
+
+# Show stored instances from MemoObject
+puts MemoObject.all_instances
